fa60e2f8bdd9049515046b86d911774dfef4e30d
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-02-26 20:41:08 +0530

    [Arun] Add: Creating common layer

---
+++ CommonLayer/RequestModels/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.RequestModels
+{
+    public class LoginModel
+    {
+        public string userEmail { get; set; }
+        public string userPassword { get; set; }
+    }
+}
---
+++ CommonLayer/RequestModels/RegisterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.RequestModels
+{
+    public class RegisterModel
+    {  
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string userEmail { get; set; }
+        public string userPassword { get; set; }
+    }
+}
