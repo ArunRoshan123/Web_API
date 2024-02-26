ec19551a32676cdfe273e6253b22f28cbc840d35
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-02-26 20:39:21 +0530

    [Arun] Add: Creating Fundo layer

---
+++ FunDo_Notes/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using CommonLayer.RequestModels;
+using CommonLayer.ResponseModel;
+using ManagerLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.Entity;
+using System;
+
+namespace FunDo_Notes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserManager userManager;
+
+        public UserController(IUserManager userManager)
+        {
+            this.userManager = userManager;
+        }
+        [HttpPost]
+        [Route("Reg")]
+        public ActionResult Register(RegisterModel model)
+        {
+            try
+            {
+                var response = userManager.UserRegisteration(model);
+                if (response != null)
+                {
+                    return Ok(new ResModel<UserEntity> { Success = true, Message = "register successfull", Data = response });
+                }
+                else
+                {
+                    return BadRequest(new ResModel<UserEntity> { Success = false, Message = "register failed", Data = response });
+                }
+            }
+            catch(Exception e)
+            {
+                return BadRequest(new ResModel<UserEntity> { Success = false, Message = e.Message, Data = null });
+            }
+        }
+
+        [HttpPost]
+        [Route("Log")]
+        public ActionResult Login(LoginModel model)
+        {
+            try
+            {
+                string response = userManager.UserLogin(model);
+                if (response != null)
+                {
+                    return Ok(new ResModel<string> { Success = true, Message = "Login sucessfull", Data = response });
+                }
+                else
+                {
+                    return BadRequest(new ResModel<string> { Success = false, Message = "Login failed", Data = response });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResModel<string> { Success = false, Message = ex.Message, Data = null });
+            }
+        }
+    }
+    
+}
---
+++ FunDo_Notes/Startup.cs
@@ -0,0 +1,71 @@
+using FunDo_Notes.Controllers;
+using ManagerLayer.Interfaces;
+using ManagerLayer.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RepositoryLayer.Context;
+using RepositoryLayer.Interfaces;
+using RepositoryLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunDo_Notes
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var strcnt = Configuration["ConnectionStrings:FunDoDD"];
+            services.AddControllers();
+            services.AddDbContext<FunNoteContext>(opts => opts.UseSqlServer(Configuration["ConnectionStrings:FunDoDD"]));
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IUserManager, UserManager>();
+
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee API V1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
