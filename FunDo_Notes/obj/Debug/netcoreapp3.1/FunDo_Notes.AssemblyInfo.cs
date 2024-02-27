837ada5bafc41e4f0a29edff18daf92e7f8bc723
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-02-27 09:11:47 +0530

    [Arun] Add: Implementing API for User Registeration

--- FunDo_Notes/obj/Debug/netcoreapp3.1/FunDo_Notes.AssemblyInfo.cs
+++ FunDo_Notes/obj/Debug/netcoreapp3.1/FunDo_Notes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FunDo_Notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4887c6edcae8a328847ae293305dfc38bc657031")]
 [assembly: System.Reflection.AssemblyProductAttribute("FunDo_Notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FunDo_Notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RepositoryLayer/obj/Debug/netcoreapp3.1/RepositoryLayer.AssemblyInfo.cs
+++ RepositoryLayer/obj/Debug/netcoreapp3.1/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec19551a32676cdfe273e6253b22f28cbc840d35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4887c6edcae8a328847ae293305dfc38bc657031")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
