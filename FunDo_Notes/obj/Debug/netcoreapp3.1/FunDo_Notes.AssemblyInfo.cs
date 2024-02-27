d0869f905ef4c99788599ca89d5d4bc64436171d
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-02-27 09:45:37 +0530

    [Arun] Add: Encrypting the password by using Bcrypt

--- FunDo_Notes/obj/Debug/netcoreapp3.1/FunDo_Notes.AssemblyInfo.cs
+++ FunDo_Notes/obj/Debug/netcoreapp3.1/FunDo_Notes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FunDo_Notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+837ada5bafc41e4f0a29edff18daf92e7f8bc723")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be1f71561b9bc2f64533d2c5a3244e54b7b1935b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FunDo_Notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FunDo_Notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
