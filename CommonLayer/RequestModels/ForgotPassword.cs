cf618c87a34853b9aa6817ad431eacf8ca1abc2d
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-02-28 16:15:52 +0530

    [Arun] Add: Implementing JWT Token on client request

---
+++ CommonLayer/RequestModels/ForgotPassword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.RequestModels
+{
+    public class ForgotPassword
+    {
+        public string userEmail { get; set; }
+        public string userId { get; set; }
+    }
+}
---
+++ CommonLayer/RequestModels/Send.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+
+namespace CommonLayer.RequestModels
+{
+    public class Send
+    {
+        public string SendMail(string ToEmail, string Token)
+        {
+            Console.WriteLine(Token);
+            string FromEmail = "arunraksha234@gmail.com";
+            MailMessage Message = new MailMessage(FromEmail, ToEmail);
+            string MailBody = "Token generated: " + Token;
+            Message.Subject = "Token generated for Forget Password";
+            Message.Body = MailBody.ToString();
+            Message.BodyEncoding = Encoding.UTF8;
+            Message.IsBodyHtml = true;
+
+            SmtpClient SmtpClient = new SmtpClient("smtp.gmail.com", 587);
+            NetworkCredential credential = new NetworkCredential("arunraksha234@gmail.com", "tjzl tyak krld iukh");
+            SmtpClient.EnableSsl = true;
+            SmtpClient.UseDefaultCredentials = true;
+            SmtpClient.Credentials = credential;
+            SmtpClient.Send(Message);
+            return ToEmail;
+        }
+    }
+}
