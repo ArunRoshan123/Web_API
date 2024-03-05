c8f22db9ae9316384c2f6bb9a0aee84ecac237f2
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-03-05 12:59:54 +0530

    [Arun] Add: Created Api for Notes

---
+++ CommonLayer/RequestModels/EditModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.RequestModels
+{
+    public class EditModel
+    {
+        public string Description { get; set; }
+        public string colour { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
--- CommonLayer/obj/Release/netcoreapp3.1/CommonLayer.AssemblyInfo.cs
+++ CommonLayer/obj/Release/netcoreapp3.1/CommonLayer.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonLayer")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38aa334116da1137f38a043041d50ca9b92cac90")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonLayer")]
